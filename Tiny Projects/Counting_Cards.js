19cfabcd8ed4b70d717bbcf27595a5e644c8b624
Angie <sacconeangela12@gmail.com>
2022-03-21 00:13:40 -0400

    Update Counting_Cards.js

--- Tiny Projects/Counting_Cards.js
+++ Tiny Projects/Counting_Cards.js
@@ -1,13 +1,15 @@
 let count = 0;
 
 function cc(card) {
- 
-  if (card > 1 && card > 7) {
+  var otherCards = ['J','Q','K','A'];
+  if (card > 1 && card < 7) {
     count++;
-  } else if (card === 10 || card === 'JQKA')
+  } else if (card === 10 || otherCards) {
     count--;
+  }
   if (count > 0) return count + " Bet";
     return count + " Hold";
-  
 }
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
 
