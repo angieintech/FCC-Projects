0b1b3fa2cc00884a9146f17d6f66bce276db49a4
Angie <sacconeangela12@gmail.com>
2022-03-25 01:07:09 -0400

    Update arithmetic_formatter.py

--- Tiny Projects/arithmetic_formatter.py
+++ Tiny Projects/arithmetic_formatter.py
@@ -2,6 +2,9 @@ def arithmetic_arranger(problems):
 
   if len(problems) > 5:
     return "Error: Too many problems."
-  def plusAndMinus():
     if op == "*" or "/":
       return "Error: Operator must be '+' or '-'."
+    if num1 != num1.isdigit() or num2 != num2.isdigit() :
+      return "Error: Numbers must only contain digits."
+    
+    
