0d99a8814c4b950e27c3e256590a041b769e2a29
Angie <sacconeangela12@gmail.com>
2022-03-25 13:04:57 -0400

    Update arithmetic_formatter.py

--- Tiny Projects/arithmetic_formatter.py
+++ Tiny Projects/arithmetic_formatter.py
@@ -6,7 +6,10 @@ def arithmetic_arranger(problems):
   lineThree = ""
   lineFour = ""
 
+  problems = []
+  
   if len(problems) > 5:
     return "Error: Too many problems."
-    
+  for i in problems:
+    firstNum, secondNum, operator = problems.split() 
     
