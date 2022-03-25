3256566f9b72d6f9a146ebffb03dc7a9ea15d781
Angie <sacconeangela12@gmail.com>
2022-03-25 00:39:11 -0400

    Create testObj.js

---
+++ Tiny Projects/testObj.js
@@ -0,0 +1,8 @@
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"];   // access the object using bracket notation.
+const drinkValue = testObj["the drink"];    
