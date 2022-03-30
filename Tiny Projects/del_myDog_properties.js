31496f43a722363cf6690d4f9470bf364a9ffb70
Angie <sacconeangela12@gmail.com>
2022-03-30 17:29:42 -0400

    Create del_myDog_properties.js

---
+++ Tiny Projects/del_myDog_properties.js
@@ -0,0 +1,9 @@
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+delete myDog.tails; //use delete keyword with dot notation
