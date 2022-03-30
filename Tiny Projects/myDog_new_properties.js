6fffeb8ffff06464ddf5707b20143da7992a4e45
Angie <sacconeangela12@gmail.com>
2022-03-30 17:27:20 -0400

    Create myDog_new_properties.js

---
+++ Tiny Projects/myDog_new_properties.js
@@ -0,0 +1,8 @@
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof"; //added bark property to myDog and set it to a sound, woof. 
