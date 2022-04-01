b1e14fb2e53dddf3db66fe200e3b568ba364f7a2
Angie <sacconeangela12@gmail.com>
2022-03-31 22:29:01 -0400

    Update checkDogObj.js

--- Tiny Projects/JS/Dogs/checkDogObj.js
+++ Tiny Projects/JS/Dogs/checkDogObj.js
@@ -1,3 +1,4 @@
-function checkObj(obj, checkProp) {
-  return "Change Me!";
+function checkDogObj(obj, checkProp) {
+  // Used a one-line if-else statement in JS :)
+  return (obj.hasOwnProperty(checkProp) ? obj[checkProp] : "Not Found");
 }
