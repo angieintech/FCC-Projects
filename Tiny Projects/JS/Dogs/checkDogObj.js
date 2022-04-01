b93ac938876b064761fa167040949f43af530422
Angie <sacconeangela12@gmail.com>
2022-03-31 23:20:15 -0400

    Update checkDogObj.js

--- Tiny Projects/JS/Dogs/checkDogObj.js
+++ Tiny Projects/JS/Dogs/checkDogObj.js
@@ -1,7 +1,7 @@
 function checkDogObj(obj, checkProp) {
-  // Used a one-line if-else statement in JS :)
-  // They both return output. 
-  // If the object has checkProp passed to the function (obj) and it has a specific property (checkProp). 
-  // If we discover property then return that property's value. Otherwise, return "Not Found".
+  /* Used a one-line if-else statement in JS :)
+   They both return output. 
+   If the object has checkProp passed to the function (obj) and it has a specific property (checkProp). 
+   If we discover property then return that property's value. Otherwise, return "Not Found". */
   return (obj.hasOwnProperty(checkProp) ? obj[checkProp] : "Not Found");
 }
