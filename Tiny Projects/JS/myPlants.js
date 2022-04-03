195b3689ee3975da371ec59e57ee499e2136e058
Angie <sacconeangela12@gmail.com>
2022-04-02 23:23:58 -0400

    Accessed a nested array using dot and bracket notation.

---
+++ Tiny Projects/JS/myPlants.js
@@ -0,0 +1,20 @@
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1]; // Accessed a nested array using dot and bracket notation. This specific one accessed the pine tree. 
