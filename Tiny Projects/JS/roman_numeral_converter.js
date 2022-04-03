645ed76d61f5c8af41aec1b33c5901c736dfee8c
Angie <sacconeangela12@gmail.com>
2022-04-02 23:35:24 -0400

    Update roman_numeral_converter.js

--- Tiny Projects/JS/roman_numeral_converter.js
+++ Tiny Projects/JS/roman_numeral_converter.js
@@ -1,5 +1,19 @@
 function convertToRoman(num) {
- return num;
+  var val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+  
+  var syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+  
+  roman_numeral = "";
 }
 
 convertToRoman(36);
