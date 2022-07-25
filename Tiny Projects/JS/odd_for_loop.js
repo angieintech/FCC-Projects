1300828fe3e36c45189eb0d0fb084d49525789b1
Angie <sacconeangela12@gmail.com>
2022-07-24 22:41:25 -0400

    Create odd_for_loop.js

---
+++ Tiny Projects/JS/odd_for_loop.js
@@ -0,0 +1,7 @@
+//for loop to print out odd numbers
+const myArray = [];
+const myArray = [];
+for (let j = 1; j < 10; j += 2) { //make j = 1 as the starter
+  myArray.push(j);
+}
+//output: [1,3,5,7,9]
