f62d21fd10054171acd9b59da42f7496704a9c67
Angie <sacconeangela12@gmail.com>
2022-07-24 21:48:16 -0400

    Update for_loop.js

--- Tiny Projects/JS/for_loop.js
+++ Tiny Projects/JS/for_loop.js
@@ -1,6 +1,5 @@
 //Goal: print values 1 - 5 by using a for loop
 const myArray = [];
-const myArray = [];
 for (let j = 1; j <= 5; j++) { //j = 1 is our starter; j<=5 will print up to 5
   myArray.push(j);
 }
