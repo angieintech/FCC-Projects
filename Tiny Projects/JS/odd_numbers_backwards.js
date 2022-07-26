08031af9bae58970011efe7ac5cc4f1c39da0760
Angie <sacconeangela12@gmail.com>
2022-07-25 22:58:28 -0400

    Create odd_numbers_backwards.js

---
+++ Tiny Projects/JS/odd_numbers_backwards.js
@@ -0,0 +1,6 @@
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+const myArray = [];
+const myArray = [];
+for (let k = 9; k > 0; k -= 2) { //k = 9 is the starter; k -=2 subtracts 2 each time. I.e.: 9 - 2 = 7 and repeats until you get to value, 1.
+  myArray.push(k);
+} //output: [9,7,5,3,1]
