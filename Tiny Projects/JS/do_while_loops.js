bc6d3505103b3c2730b8f81815214cdcdb7a02dd
Angie <sacconeangela12@gmail.com>
2022-07-26 00:12:41 -0400

    Create do_while_loops.js

---
+++ Tiny Projects/JS/do_while_loops.js
@@ -0,0 +1,12 @@
+/* Change the while loop in the code to a do...
+while loop so the loop will push only the number 10 to myArray, 
+and i will be equal to 11 when your code has finished running. */
+
+const myArray = [];
+let i = 10;
+
+do {
+  myArray.push(i); //I.e.: do this action while the condition i < 5. This will only run until i is no longer less than 5.
+  i++;
+  }
+  while (i < 5);
