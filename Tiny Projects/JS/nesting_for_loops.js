32cc25e72e9a661b297b611bafad6c350e9f40a9
Angie <sacconeangela12@gmail.com>
2022-07-25 23:47:28 -0400

    Create nesting_for_loops.js

---
+++ Tiny Projects/JS/nesting_for_loops.js
@@ -0,0 +1,10 @@
+// Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr
+function multiplyAll(arr) {
+  let product = 1;
+ 
+
+
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
