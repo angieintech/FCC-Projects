9a2b6d5eece09efd94476e2d78d277cf4f2eb21e
Angie <sacconeangela12@gmail.com>
2022-07-31 23:21:55 -0400

    Update random_tiny_fractions.js

--- Tiny Projects/JS/random_tiny_fractions.js
+++ Tiny Projects/JS/random_tiny_fractions.js
@@ -1,5 +1,4 @@
 // Change randomFraction to return a random number instead of returning 0.
 function randomFraction() {
-  
   return Math.random(); //use Math.random() to return a random number instead of 0.
 }
