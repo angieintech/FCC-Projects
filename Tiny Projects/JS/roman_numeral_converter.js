2f0ab07d80cda1c5299b99cc1e65df8356bd4162
Angie <sacconeangela12@gmail.com>
2023-02-03 00:23:26 -0500

    Update roman_numeral_converter.js

--- Tiny Projects/JS/roman_numeral_converter.js
+++ Tiny Projects/JS/roman_numeral_converter.js
@@ -1,23 +1,19 @@
 function convertToRoman(num) {
-  let val = [
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4,
-            1
-            ]
   
-  let syb = [
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV",
-            "I"
-            ]
-  
-  let roman_numeral = "";
-}
+  let sw_arabic_roman = {1: "I", 2: "II", 3: "III",  4 : "IV", 5: "V", 6: "VI", 7 : "VII", 8 : "VIII", 9 : "IX", 
+10 : "X", 20 : "XX", 30 : "XXX", 40 : "XL", 50 : "L", 60 : "LX", 70 : "LXX", 80 : "LXXX", 90 : "XC", 
+100 : "C", 200 : "CC", 300 : "CCC", 400 : "CD", 500 : "D", 600 : "DC", 700 : "DCC", 800 : "DCCC", 900 : "CM", 
+1000: "M", 2000: "MM", 3000: "MMM"} 
+            
 
+let num1 = String(+ num);
+//let numbers = [...num1]; //num1.split()
+  
+//empty result
+let roman_numeral = "";
+  
   while (num > 0) {
-    for (var i = 0; i < i.length; i++) {
+    for (var i = 0; i < numbers.length; i++) {
       // Then convert arabic numerals to roman numerals using += and -
     }
   }
